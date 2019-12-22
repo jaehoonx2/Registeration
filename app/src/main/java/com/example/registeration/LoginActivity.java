9b0ea755c3d7750ddad92498db8b3092e1698928
jaehoonx2 <21300698@handong.edu>
2019-12-22 15:10:49 +0900

    Register Activity

--- app/src/main/java/com/example/registeration/LoginActivity.java
+++ app/src/main/java/com/example/registeration/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.registeration;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView registerButton = (TextView) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
